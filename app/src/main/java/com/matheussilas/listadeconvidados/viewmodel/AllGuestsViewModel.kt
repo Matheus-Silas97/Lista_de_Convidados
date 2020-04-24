f4fc1f182dce8a8148bfa90792e2ac0a27cfd8d8
Matheus Silas <54551805+Matheus-Silas97@users.noreply.github.com>
2020-04-24 12:20:34 -0300

    Initial commit

---
+++ app/src/main/java/com/matheussilas/listadeconvidados/viewmodel/AllGuestsViewModel.kt
@@ -0,0 +1,13 @@
+package com.matheussilas.listadeconvidados.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class AllGuestsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is All Guests Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/com/matheussilas/listadeconvidados/viewmodel/GuestFormViewModel.kt
@@ -0,0 +1,9 @@
+package com.matheussilas.listadeconvidados.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class GuestFormViewModel : ViewModel(){
+
+
+
+}
