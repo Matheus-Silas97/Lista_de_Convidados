25aba1ade41fc710e0e5e17d6ce481648696980e
Matheus Silas <54551805+Matheus-Silas97@users.noreply.github.com>
2020-04-24 13:01:23 -0300

    create repository

---
+++ app/src/main/java/com/matheussilas/listadeconvidados/service/model/GuestModel.kt
@@ -0,0 +1,5 @@
+package com.matheussilas.listadeconvidados.service.model
+
+import android.text.BoringLayout
+
+data class GuestModel (var name: String, var presence: Boolean)
--- app/src/main/java/com/matheussilas/listadeconvidados/viewmodel/GuestFormViewModel.kt
+++ app/src/main/java/com/matheussilas/listadeconvidados/viewmodel/GuestFormViewModel.kt
@@ -1,9 +1,25 @@
 package com.matheussilas.listadeconvidados.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.matheussilas.listadeconvidados.service.model.GuestModel
+import com.matheussilas.listadeconvidados.service.repository.GuestRepository
 
-class GuestFormViewModel : ViewModel(){
+class GuestFormViewModel : ViewModel() {
 
+    private val mGuestRepository: GuestRepository =
+        GuestRepository()
 
+    private var mSaveGuest = MutableLiveData<Boolean>()
+    val saveGuest: LiveData<Boolean> = mSaveGuest
+
+    fun save(name: String, presence: Boolean) {
+        val guest = GuestModel(
+            name,
+            presence
+        )
+        mGuestRepository.save(guest)
+    }
 
 }
